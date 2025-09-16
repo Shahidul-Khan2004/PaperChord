e1a69bf6669b2ac82990ae5218623e2564fee3ee
Shahidul <theshahidkhan.2004@gmail.com>
2025-09-17 01:00:48 +0600

    added typehints and comments

--- project.py
+++ project.py
@@ -5,7 +5,8 @@ def main():
     text = input("Enter your text: ")
     print(add_keywords_to_sentiment(analyze_sentiment(text)))
 
-def analyze_sentiment(text):
+def analyze_sentiment(text: str) -> str:
+    """Analyze the sentiment of the given text and return 'Positive', 'Negative', or 'Neutral'."""
     analyzer = SentimentIntensityAnalyzer()
     score = analyzer.polarity_scores(text)
     if score["compound"] >= 0.25:
@@ -15,10 +16,11 @@ def analyze_sentiment(text):
     else:
         return "Neutral"
 
-def add_keywords_to_sentiment(text):
-    if text == "Positive":
+def add_keywords_to_sentiment(sentiment: str) -> str:
+    """Assign a random keyword based on the relevant sentiment."""
+    if sentiment == "Positive":
         return random.choice(["good mood", "happy", "chill vibes", "feel good", "relaxing"])
-    elif text == "Negative":
+    elif sentiment == "Negative":
         return random.choice(["sad song", "blue", "heart break", "memories"])
     else:
         return random.choice(["inspo", "lofi", "nature", "mood", "calm"])
