9d143ba0e9771c635f9928f104194010864ab49a
Shahidul <theshahidkhan.2004@gmail.com>
2025-09-17 00:43:09 +0600

    got sentiment working

--- project.py
+++ project.py
@@ -1,9 +1,18 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-analyzer = SentimentIntensityAnalyzer()
+def main():
+    text = input("Enter your text: ")
+    print(analyze_sentiment(text))
 
-text = input("Enter your text: ")
+def analyze_sentiment(text):
+    analyzer = SentimentIntensityAnalyzer()
+    score = analyzer.polarity_scores(text)
+    if score['compound'] >= 0.25:
+        return "Positive"
+    elif score['compound'] <= -0.25:
+        return "Negative"
+    else:
+        return "Neutral"
 
-score = analyzer.polarity_scores(text)
-
-print("Sentiment Score:", score["compound"])
+if __name__ == "__main__":
+    main()
