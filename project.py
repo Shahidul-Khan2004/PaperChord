e55f9aa3c02cd893fd3468dda3e3a59048a608d5
Shahidul <theshahidkhan.2004@gmail.com>
2025-09-17 00:57:50 +0600

    keywords from sentiments

--- project.py
+++ project.py
@@ -1,18 +1,27 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import random
 
 def main():
     text = input("Enter your text: ")
-    print(analyze_sentiment(text))
+    print(add_keywords_to_sentiment(analyze_sentiment(text)))
 
 def analyze_sentiment(text):
     analyzer = SentimentIntensityAnalyzer()
     score = analyzer.polarity_scores(text)
-    if score['compound'] >= 0.25:
+    if score["compound"] >= 0.25:
         return "Positive"
-    elif score['compound'] <= -0.25:
+    elif score["compound"] <= -0.25:
         return "Negative"
     else:
         return "Neutral"
+
+def add_keywords_to_sentiment(text):
+    if text == "Positive":
+        return random.choice(["good mood", "happy", "chill vibes", "feel good", "relaxing"])
+    elif text == "Negative":
+        return random.choice(["sad song", "blue", "heart break", "memories"])
+    else:
+        return random.choice(["inspo", "lofi", "nature", "mood", "calm"])
 
 if __name__ == "__main__":
     main()
