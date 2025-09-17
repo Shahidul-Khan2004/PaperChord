525cd45c1d1a6f78b1a91349ac9aeb90d7d5410c
Shahidul <theshahidkhan.2004@gmail.com>
2025-09-18 01:36:24 +0600

    added itunes api for response

--- project.py
+++ project.py
@@ -1,9 +1,10 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import random
+import requests
 
 def main():
     text = input("Enter your text: ")
-    print(add_keywords_to_sentiment(analyze_sentiment(text)))
+    print(fetch_suggestions(add_keywords_to_sentiment(analyze_sentiment(text))))
 
 def analyze_sentiment(text: str) -> str:
     """Analyze the sentiment of the given text and return 'Positive', 'Negative', or 'Neutral'."""
@@ -24,6 +25,23 @@ def add_keywords_to_sentiment(sentiment: str) -> str:
         return random.choice(["sad song", "blue", "heart break", "memories"])
     else:
         return random.choice(["inspo", "lofi", "nature", "mood", "calm"])
+
+def fetch_suggestions(str: str) -> list:
+    """Fetch song suggestions from the iTunes API based on the given keyword."""
+    songs = []
+    response = requests.get(f"https://itunes.apple.com/search?term={str}&media=music&entity=song&limit=3")
+    json = response.json()
+    for result in json["results"]:
+        songs.append(
+            {
+                "trackName": result["trackName"],
+                "artistName": result["artistName"],
+                "trackViewUrl": result["trackViewUrl"],
+                "artworkUrl100": result["artworkUrl100"],
+            }
+        )
+    return songs
+
 
 if __name__ == "__main__":
     main()
